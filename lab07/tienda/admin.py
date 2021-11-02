16f6d6061631090d0848aff2333a7938c44a38f6
edisonrosas <edison.rosas.h@tecsup.edu.pe>
2021-11-01 21:16:21 -0500

    lab09

--- lab07/tienda/admin.py
+++ lab07/tienda/admin.py
@@ -8,3 +8,13 @@ class ProductoAdmin(admin.ModelAdmin):
 
 admin.site.register(Producto)
 #admin.site.register(Cliente)
+#class ProductoAdmin(admin.ModelAdmin):
+#    list_display={'nombre'}#
+
+#admin.site.register(Producto)
+#admin.site.register(Noticia, NoticiaAdmin)
+#class ProductoAdmin(admin.ModelAdmin):
+#    prepopulated_fields = {'slug': ('titulo',),}
+
+#class ProductoAdmin(admin.ModelAdmin):
+#    prepopulated_fields = {'nombre':nombre}
